444cf26832ae5eacba81ef93de8dfff6b48054d3
Brent Pedersen <bpederse@gmail.com>
2018-08-07 10:31:46 -0600

    add nim.cfg

---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+set -euo pipefail
+
+NIM_VERSION=v0.17.2
+NIMBLE_VERSION=v0.8.10
+
+base=$(pwd)
+
+git clone -b $NIM_VERSION --depth 1 git://github.com/nim-lang/nim nim-$NIM_VERSION/
+cd nim-$NIM_VERSION
+git clone -b $NIM_VERSION --depth 1 git://github.com/nim-lang/csources csources/
+
+cd csources
+sh build.sh
+cd ..
+rm -rf csources
+bin/nim c koch
+./koch boot -d:release
+
+export PATH=$PATH:$base/nim-$NIM_VERSION/bin/:$PATH:$base/nimble/src
+
+cd $base
+
+git clone -b $NIMBLE_VERSION --depth 1 git://github.com/nim-lang/nimble.git
+cd nimble
+nim c src/nimble
+cp ./src/nimble /usr/bin/
+
+cd $base
+git clone --depth 1 git://github.com/brentp/hts-nim.git
+cd hts-nim
+grep -v requires hts.nimble > k.nimble && mv k.nimble hts.nimble
+nimble install -y
+
+cd $base
+git clone --depth 1 git://github.com/brentp/somalier.git
+cd somalier
+nim c --threads:on -d:release src/somalier.nim 
+cp ./somalier /io
