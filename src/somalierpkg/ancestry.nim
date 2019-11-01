fa5003e43ac647d76e23043e83387e8324711fa0
Brent Pedersen <bpederse@gmail.com>
2019-11-01 14:43:00 -0600

    aesthetics and iters

--- src/somalierpkg/ancestry.nim
+++ src/somalierpkg/ancestry.nim
@@ -122,7 +122,7 @@ proc ancestry_main*() =
     T = train_mat.toTensor()
     Y = int_labels.toTensor() #.astype(float32)#.unsqueeze(0).transpose
     t0 = cpuTime()
-    res = T.pca(nPCs, center=true, n_power_iters=4)
+    res = T.pca(nPCs, center=true) #, n_power_iters=4)
 
   stderr.write_line &"[somalier] time for dimensionality reduction to shape {res.projected.shape}: {cpuTime() - t0:.2f} seconds"
 
@@ -150,7 +150,7 @@ proc ancestry_main*() =
   t0 = cpuTime()
 
   # train the model
-  for epoch in 0..<500:
+  for epoch in 0..<1000:
 
     for batch_id in 0..<X.value.shape[0] div batch_size:
 
