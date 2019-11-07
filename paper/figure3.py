b1a4e127afa951173ca0f343dbbdb56efd38e655
Brent Pedersen <bpederse@gmail.com>
2019-11-07 13:57:43 -0700

    commit changes for figure

--- paper/figure3.py
+++ paper/figure3.py
@@ -3,6 +3,10 @@ import sys
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
+import matplotlib.font_manager
+
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'arial'
 
 colors = sns.color_palette("Set2")
 
@@ -17,17 +21,18 @@ print(s)
 
 for i, ex_rel in enumerate(df.expected_relatedness.unique()):
     sub = df.loc[df.expected_relatedness == ex_rel, :]
-    plt.scatter(sub.ibs0, sub.ibs2, label="unrelated" if ex_rel == -1 else
-            "identical", ec="none", c=colors[i], s=s)
+    plt.scatter(sub.ibs0, sub.ibs2, label="Unrelated" if ex_rel == -1 else
+            "Identical", ec="none", c=colors[i], s=s)
 
-sub = df.loc[df.ibs0 < 600]
-plt.scatter(sub.ibs0, sub.ibs2, label="unexpected relatedness",
+sub = df.loc[df.ibs0 < 450]
+plt.scatter(sub.ibs0, sub.ibs2, label="Unexpected relatedness",
         ec="k", c=colors[0], s=s+4)
 
 plt.xlabel("Number of IBS0 sites")
 plt.ylabel("Number of IBS2 sites")
 
 sns.despine()
-plt.legend(title="expected relatedness")
-plt.savefig("somalier-figure3.eps")
-plt.show()
+plt.legend(frameon=False)
+#plt.savefig("somalier-figure3.eps")
+plt.savefig("somalier-figure3.png")
+#plt.show()
