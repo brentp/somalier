c6add0159ed4f1a53a1967fa208c06e1343968b2
Brent Pedersen <bpederse@gmail.com>
2026-04-15 13:37:42 -0700

    remove unused files

--- eval.sh
+++
@@ -1,2 +0,0 @@
-nim c -d:danger src/somalier.nim
-DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib:/opt/homebrew/opt/openblas/lib ./src/somalier relate ../somalier_out/*.somalier -o isabl-test -g ../grps.txt
